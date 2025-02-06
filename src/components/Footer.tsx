a7d19e240683fc7295761de7c86db7ad8b0626d6
samrawaseem <160887316+samrawaseem@users.noreply.github.com>
2025-02-06 20:10:16 +0500

    Initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2025 Samra Waseem. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>Samra Waseem</div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
